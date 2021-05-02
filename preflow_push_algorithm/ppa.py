a3cf6c094e61f40a625c21fdee8f8057b668d467
Ilya Katun <katun.ilya@gmail.com>
2021-05-02 18:14:46 +0300

    #1 add init func; excess_flow func; preflow func

--- preflow_push_algorithm/ppa.py
+++ preflow_push_algorithm/ppa.py
@@ -1,46 +1,65 @@
 import networkx as nx
-from networkx.classes.graph import Graph
-from networkx.classes.reportviews import NodeView
 
-class PPA():
-    """Class that provides an interface to the preflow push algorithm."""
 
-    def __init__(self, graph: Graph) -> None:
-        """Initialize PPA class.
+def initialize_network(raw_data):
+    """Creates networkx's DiGraph for PPA Algo
 
-        Args:
-            graph (Graph): graph.
-        """
-        self.graph = graph
+    Args:
+        raw_data (dict): raw graph data
+    """
 
-    def __excess(self, u: NodeView) -> float:
-        """Calculate the excess flow entering the vertex u.
+    network = nx.DiGraph(source=raw_data['source'], runoff=raw_data['runoff'])
 
-        Args:
-            u (NodeView): u vertex.
+    for edge in raw_data['edges']:
+        network.add_edge(
+            edge['u'],
+            edge['v'],
+            capacity=edge['capacity'],
+            # TODO : maybe add information about reverse_preflow
+            preflow=edge['capacity'] if edge['u'] == raw_data['source'] else 0.0)
 
-        Returns:
-            float: excess flow.
-        """
-        excess_flow = 0
-        for (v1, v2, wt) in self.graph.edges.data('weight'):
-            if v2 == u.data():
-                excess_flow += wt['flow']
-        return excess_flow
-            
+    node_num = len(network.nodes)
+    enriched_nodes = {}
 
-    def push(self, u: NodeView, v: NodeView) -> None:
-        """Pushes the preflow from vertex u to vertex v.
+    for node in network.nodes:
+        enriched_nodes[node] = {
+            'vertex_label': node_num if node == network.graph['source'] else 0.0,
+            'excess_flow': excess_flow_of(network, node)
+        }
 
-        Args:
-            u (NodeView): u vertex.
-            v (NodeView): v vertex.
-        """
-        pass
+    nx.set_node_attributes(network, enriched_nodes)
 
-    def ppa() -> float:
-        """Find the maximum flow in the graph.
+    return network
 
-        Returns:
-            float: maximum flow value.
-        """
+
+def excess_flow_of(graph: nx.DiGraph, u):
+    """Calculates excess flow for node u in graph
+
+    Args:
+        graph (nx.DiGraph): source graph 
+        u ([type]): node in graph for which we calculate excess flow
+
+    Returns:
+        float: excess flow value. If it's positive than node is overflowing
+    """
+    return sum([preflow_of(graph, v, u) for v in graph.nodes])
+
+
+def preflow_of(graph: nx.DiGraph, u, v):
+    """Recalculates preflow for edge (as we do not store 2 way edges)
+
+    Args:
+        graph (nx.DiGraph): network
+        u (str): start edge node
+        v (str): end edge node
+
+    Returns:
+        [float]: preflow value for some edge (with direction info)
+    """
+    # TODO : theroetically might be optimized
+    if graph.has_edge(u, v):
+        return graph[u][v]['preflow']
+    elif graph.has_edge(v, u):
+        return -1 * graph[v][u]['preflow']
+    else:
+        return 0.0
